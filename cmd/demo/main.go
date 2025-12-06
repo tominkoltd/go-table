ceeddb5ac6b50a2cad757e312987e59cda95a69a
tominkoltd <info@tominko.com>
2025-12-06 21:04:30 +0000

    Initial commit

---
+++ cmd/demo/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "github.com/tominkoltd/go-table"
+)
+
+func main() {
+    // call anything you want to test
+    //table.DoSomething()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tominkoltd/go-table
+
+go 1.25.4
